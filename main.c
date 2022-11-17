f2c1e6cb581ea77d70a5fc397cb116d64835a242
Julia Gersey <jgersey20@bw.edu>
2022-11-17 13:51:01 -0500

    project structure with comments

--- main.c
+++ main.c
@@ -6,3 +6,17 @@ int main() {
     printf("let's try this");
     return 0;
 }
+
+// get input here
+
+// read function
+// sample output:   32:contents of file
+// fgets() and keep track of how many bytes you read
+
+
+// write function
+
+
+// delete function
+
+
