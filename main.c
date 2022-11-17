cfe0e51220496e0f9f452d85ecbd45d94196118e
Julia Gersey <jgersey20@bw.edu>
2022-11-17 15:20:09 -0500

    carrie's changes

--- main.c
+++ main.c
@@ -5,26 +5,15 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-
 #define BUFFER_SIZE 256
-
 // read function
 // sample output:   32:contents of file
 // fgets() and keep track of how many bytes you read
-
-// if file exists
-    // FILE *inputStream = fopen(filename, "r");
-    // save the contents of the file to a string
-    // count number of bytes
-    // return number: contents
-// if file doesn't exist
-    // return 0:
-
-
 // write function
-
-
 // delete function
+void * delete(void * input) {
+    unlink(input);
+}
 
 int main() {
     // gets input
@@ -32,28 +21,30 @@ int main() {
     char command[32];
     char *call = malloc(7);
     fgets(command, 32, stdin);
-
     // gets function, filename, and the rest
     for (int i = 0; i < 32; i++) {
         if ((command[i] == ' ')) {
             // gets the name of function to call
-          strncpy(call, command, i);
+            strncpy(call, command, i);
+            char filename[32];
+            int count = 0;
+            while (count < strlen(command) ) {
+                filename[count] = command[(i + 2) + count - 1];
+                count++;
+            }
+            //filename[count] = '\0';
+            printf("%s", filename);
         } // gets the filename
         // gives the rest to write
     }
 
-    printf("%s", call);
     //printf("%s\n", program);
-
-    if (strcmp(call, "read") == 0) {
-        //read(filename);
-
-    } else if (strcmp(call, "write") == 0) {
+    if (strcmp(command, "read") == 0) {
+        // pass to read function
+    } else if (strcmp(command, "write") == 0) {
         // pass to write
-
     } else {
         // pass to delete
     }
 
 }
-
