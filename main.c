c0a8dc05ce18702c586251057a2f26a5f3ef3510
Julia Gersey <jgersey20@bw.edu>
2022-11-17 14:43:40 -0500

    gets function name

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
+#define BUFFER_SIZE 256
 // read function
 // sample output:   32:contents of file
 // fgets() and keep track of how many bytes you read
@@ -13,8 +18,33 @@
 // delete function
 
 int main() {
-    // get input here
+    // gets input
+    printf("enter command: ");
+    char command[32];
+    char *call = malloc(7);
+    fgets(command, 32, stdin);
 
-    return 0;
+    // gets function, filename, and the rest
+    for (int i = 0; i < 32; i++) {
+        if ((command[i] == ' ')) {
+            // gets the name of function to call
+          strncpy(call, command, i);
+        } // gets the filename
+        // gives the rest to write
+    }
+
+    printf("%s", call);
+    //printf("%s\n", program);
+
+    if (strcmp(command, "read") == 0) {
+      // pass to read function
+
+    } else if (strcmp(command, "write") == 0) {
+        // pass to write
+
+    } else {
+        // pass to delete
+    }
+
 }
 
