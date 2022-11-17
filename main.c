8f23a550c833050aad55ec0a69b0b076857d683e
Julia Gersey <jgersey20@bw.edu>
2022-11-17 13:52:32 -0500

    project outline

--- main.c
+++ main.c
@@ -1,14 +1,5 @@
 #include <stdio.h>
 
-int main() {
-    printf("final project!\n");
-    printf("your mom");
-    printf("let's try this");
-    return 0;
-}
-
-// get input here
-
 // read function
 // sample output:   32:contents of file
 // fgets() and keep track of how many bytes you read
@@ -19,4 +10,9 @@ int main() {
 
 // delete function
 
+int main() {
+    // get input here
+
+    return 0;
+}
 
