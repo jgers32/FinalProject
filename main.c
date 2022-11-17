342f73ea3553a1a000cb58a5c7e6df291c6bdfce
Carrie Eierman <ceierman19@bw.edu>
2022-11-17 17:13:31 -0500

    Cleaned up code

--- main.c
+++ main.c
@@ -5,47 +5,57 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+
 #define BUFFER_SIZE 256
+
+// note: strcat for the config file
+
 // read function
 // sample output:   32:contents of file
 // fgets() and keep track of how many bytes you read
+
 // write function
+// fputs()
+
 // delete function
 void delete(char * input) {
     unlink(input);
 }
 
 int main() {
-    // gets input
-    printf("enter command: ");
     char command[32];
     char *call = malloc(7);
+    char filename[32];
+
+    // gets input
+    printf("Enter command: ");
     fgets(command, 32, stdin);
-    // gets function, filename, and the rest
+
+    // gets function to call, filename, and the rest
     for (int i = 0; i < 32; i++) {
         if ((command[i] == ' ')) {
             // gets the name of function to call
             strncpy(call, command, i);
-            char filename[32];
+
+            // gets filename
             int count = 0;
             while (count < strlen(command) ) {
                 filename[count] = command[(i + 2) + count - 1];
                 count++;
             }
-            filename[count] = '\0';
+            //filename[count] = '\0';
             printf("%s", filename);
-            delete(filename);
-        } // gets the filename
+        }
         // gives the rest to write
     }
 
-    //printf("%s\n", program);
     if (strcmp(command, "read") == 0) {
         // pass to read function
     } else if (strcmp(command, "write") == 0) {
         // pass to write
     } else {
         // pass to delete
+        delete(filename);
     }
 
 }
