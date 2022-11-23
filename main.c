e8b0d6428e980dafa7080ef645784d7458114c09
Carrie Eierman <ceierman19@bw.edu>
2022-11-22 20:30:52 -0500

    concatenated filename with path

--- main.c
+++ main.c
@@ -1,34 +1,38 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
 
 #define BUFFER_SIZE 256
 
 // read function
 void readFile(char * filename) {
     char * string = malloc(BUFFER_SIZE);
-    for(int i = 0;; i++) {
-        if(filename[i] == '\n') {
+
+    for (int i = 0;; i++) {
+        if (filename[i] == '\n') {
             filename[i] = '\0';
             break;
         }
     }
-    FILE *stream = fopen(filename, "r");
+
+    FILE * stream = fopen(filename, "r");
     int fileLength = 0;
     char file[1024];
     file[0] = '\0';
+
     while (fgets(string, BUFFER_SIZE, stream)) {
         fileLength += strlen(string);
         strcat(file, string);
     }
+
     printf("%d:%s", fileLength, file);
     fclose(stream);
 }
 
 // write function
-void saveFile(char * filename, char * rest){
-    FILE *goHere = fopen(filename, "w");
+void saveFile(char * filename, char * rest, char * path) {
+    strcat(filename, path);
+    FILE * goHere = fopen(filename, "w");
     char * size = malloc(2);
     char * toSave = malloc(32);
 
@@ -36,39 +40,41 @@ void saveFile(char * filename, char * rest){
         if (rest[i] == ':') {
             strncpy(size, rest, i);
             size[i] = '\0';
+
             int count = 0;
             while (count < strlen(rest) - strlen(size) - 1) {
                 toSave[count] = rest[(i + 2) + count - 1];
                 count++;
             }
+
             toSave[count] = '\0';
             break;
         }
     }
+
     fputs(toSave, goHere);
 }
 
 // delete function
-void delete(char *input) {
+void delete(char * input) {
     unlink(input);
 }
 
 int main() {
     char * string = malloc(BUFFER_SIZE);
-    FILE *config = fopen("fs.cfg", "r");
+    FILE * config = fopen("fs.cfg", "r");
     char save_dir[1024];
     save_dir[0] = '\0';
+
     while (fgets(string, BUFFER_SIZE, config)) {
         strcat(save_dir, string);
     }
 
-    char *path = malloc(BUFFER_SIZE);
+    char * path = malloc(BUFFER_SIZE);
     for (int i = 0; i < 32; i++) {
         if (save_dir[i] == '=' && save_dir[i + 1] == ' ') {
-
-            //strncpy(path, save_dir, i);
             int count = 0;
-            while (count < strlen(save_dir) ) {
+            while (count < strlen(save_dir)) {
                 path[count] = save_dir[i + 2];
                 count++;
                 i++;
@@ -79,11 +85,11 @@ int main() {
     }
 
     char command[BUFFER_SIZE];
-    char *call = malloc(7);
-    char *filename = malloc(32);
-    char *contents = malloc(32);
+    char * call = malloc(7);
+    char * filename = malloc(32);
+    char * contents = malloc(32);
 
-    // gets input -- in future this will be from the config file
+    // gets input -- in future this will be from the server?
     printf("Enter command: ");
     fgets(command, BUFFER_SIZE, stdin);
 
@@ -92,24 +98,29 @@ int main() {
         if (command[i] == ' ') {
             strncpy(call, command, i);
             call[i] = '\0';
+
             int count = 0;
-            while (count < strlen(command) ) {
+            while (count < strlen(command)) {
                 filename[count] = command[(i + 2) + count - 1];
                 count++;
             }
+
             command[i] = '\0';
             break;
         }
     }
+
     // gets the contents for the write function
     for (int i = 0; i < 32; i++) {
         if (filename[i] == ' ') {
             strncpy(contents, filename, i);
+
             int count = 0;
-            while (count < strlen(filename) ) {
+            while (count < strlen(filename)) {
                 contents[count] = filename[(i + 2) + count - 1];
                 count++;
             }
+
             filename[i] = '\0';
             break;
         }
@@ -119,10 +130,10 @@ int main() {
         // pass to read function
         readFile(filename);
     } else if (strcmp(command, "write") == 0) {
-        // pass to write
-        saveFile(filename, contents);
+        // pass to write function
+        saveFile(filename, contents, path);
     } else {
-        // remove newline character and pass to delete
+        // remove newline character and pass to delete function
         for (int i = 0; i < 32; i++) {
             if (filename[i] == '\n') {
                 filename[i] = '\0';
